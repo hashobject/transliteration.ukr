9a99f2b5f2610954d9baaf2767ca97481b530e3c
Anton Podviaznikov <podviaznikov@gmail.com>
2015-06-30 23:32:27 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :url "https://github.com/hashobject/transliterate.ukr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
